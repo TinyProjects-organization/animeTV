7b6e17666db2eb45cb05ab9f8f405aa75d762c25
altasegemen <49123561+altasegemen@users.noreply.github.com>
2020-01-07 04:03:04 +0300

    1)kayıt sayfası oluşturuldu 2)Giriş sayfasından kayıt sayfasına geçiş sağlandı. 3)Homescreen eklendi not: kayıt sayfası düzenlenecek.

---
+++ app/src/main/java/com/coders/animetv/Homescreen/homescreen.kt
@@ -0,0 +1,13 @@
+package com.coders.animetv.Homescreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.coders.animetv.R
+
+class homescreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homescreen)
+    }
+}
