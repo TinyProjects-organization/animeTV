46806c4797130936e96711e2295ee6da143344c0
nasifcan <lasthief93@gmail.com>
2020-01-09 17:32:58 +0300

    tüm düzenlemeler yapıldı hatalar giderildi

--- app/src/main/java/com/coders/animetv/Homescreen/HomeScreen.kt
+++ app/src/main/java/com/coders/animetv/Homescreen/HomeScreen.kt
@@ -3,11 +3,27 @@ package com.coders.animetv.Homescreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.coders.animetv.R
+import com.coders.animetv.Utilz.BottomNavigationViewManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx
+import kotlinx.android.synthetic.main.activity_homescreen.*
 
 class HomeScreen : AppCompatActivity() {
+        // hangisinde olduğunu göstermek için //
+             private val  ACTIVITY_NUMBER = 0
+            private val TAG = "Home Activity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homescreen)
+        setupNavigationView()
+    }
+
+    fun setupNavigationView() {
+        BottomNavigationViewManager.setupBottomNavigationView(bottomNavigationView)
+        BottomNavigationViewManager.setupNavigation(this,bottomNavigationView)
+        var menu = bottomNavigationView.menu
+        var menuItem = menu.getItem(ACTIVITY_NUMBER)
+        menuItem.setChecked(true)
     }
 }
