9ce325a655f3a704cb6b96a09af6874796d89607
altasegemen <49123561+altasegemen@users.noreply.github.com>
2020-01-18 18:25:35 +0300

    Profil sayfası dizayn edildi ve fragmentleri oluşturuldu.

---
+++ app/src/main/java/com/coders/animetv/Profile/editprofileFragment.kt
@@ -0,0 +1,26 @@
+package com.coders.animetv.Profile
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.coders.animetv.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class editprofileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_editprofile2, container, false)
+    }
+
+
+}
